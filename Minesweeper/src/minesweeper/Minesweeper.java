3f84f07ee2bd59eb4f2e6ed80562802d1d71ff1b
hkmate <mail.kmate@gmail.com>
2015-03-04 17:43:55 +0100

    Netbeans project initiation
    Comment:
    Please set Tools/Edior/On Save/All Languages
    /Reformat (and Remove trailing whitespace) to "All Line" option in your
    netbeans.

---
+++ Minesweeper/src/minesweeper/Minesweeper.java
@@ -0,0 +1,16 @@
+package minesweeper;
+
+/**
+ *
+ * @author kmate
+ */
+public class Minesweeper {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
