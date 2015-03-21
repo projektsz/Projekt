60015bca159b0778abfbb805206f2e5a2c45e85b
Gábor Boros <gaboros92@gmail.com>
2015-03-21 14:33:18 +0100

    Maven build system configuration

--- Minesweeper/src/minesweeper/Minesweeper.java
+++ Minesweeper/src/minesweeper/Minesweeper.java
@@ -10,7 +10,7 @@ public class Minesweeper {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+		System.out.println("Hello Maven Build System! (Or not)");
     }
 
 }
