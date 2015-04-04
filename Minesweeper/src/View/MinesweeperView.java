ad8439714fef5c3f87dc96a564cc91ea8edf96f5
Balázs András <baauaci@inf.elte.hu>
2015-04-04 15:14:26 +0200

    Statisztika-bővítés
    Létrehoztam a statisztika menü gombjait, a Statistics osztály
    függvényeihez igazodva.

--- Minesweeper/src/View/MinesweeperView.java
+++ Minesweeper/src/View/MinesweeperView.java
@@ -70,14 +70,22 @@ public class MinesweeperView extends JFrame{
 
     private void setupMenu() {
         JMenuBar menuBar = new JMenuBar();
-        JMenu menu, submenu;
+        JMenu menu1, statMenu, submenu;
+        JMenu rekorderMenu, averageTimeMenu, winnerCountMenu, mostWinnerMenu;
         JMenuItem menuItem;
+        
+        JMenuItem rekorderItem1, rekorderItem2, rekorderItem3;
+        JMenuItem averageTimeItem;
+        JMenuItem winnerCountItem1, winnerCountItem2, winnerCountItem3, winnerCountItem4;
+        JMenuItem mostWinnerItem1, mostWinnerItem2, mostWinnerItem3, mostWinnerItem4;
+        
         JRadioButtonMenuItem rbMenuItem;
         JCheckBoxMenuItem cbMenuItem;
+//        Statistics.getInstance()
 
         //Build the first menu.
-        menu = new JMenu("A Menu");
-        menuBar.add(menu);
+        menu1 = new JMenu("A Menu");
+        menuBar.add(menu1);
 
         //a submenu
        // menu.addSeparator();
@@ -88,16 +96,60 @@ public class MinesweeperView extends JFrame{
 
         menuItem = new JMenuItem("10×10");
         submenu.add(menuItem);
-        menu.add(submenu);
+        menu1.add(submenu);
         
         menuItem = new JMenuItem("15×15");
         submenu.add(menuItem);
-        menu.add(submenu);
+        menu1.add(submenu);
 
         //Build second menu in the menu bar.
-        menu = new JMenu("Statisztika");
-        menuBar.add(menu);
+        statMenu = new JMenu("Statisztika");
+        menuBar.add(statMenu);
+        //rekorder submenu
+        rekorderMenu = new JMenu("Rekorderek");
+        statMenu.add(rekorderMenu);
         
+        rekorderItem1 = new JMenuItem("Rekorder (5x5)");
+        rekorderItem2 = new JMenuItem("Rekorder (10x10)");
+        rekorderItem3 = new JMenuItem("Rekorder (15x15)");
+        
+        rekorderMenu.add(rekorderItem1);
+        rekorderMenu.add(rekorderItem2);
+        rekorderMenu.add(rekorderItem3);        
+        //average time submenu
+        averageTimeMenu = new JMenu("Átlagos idők");
+        statMenu.add(averageTimeMenu);
+        
+        averageTimeItem = new JMenuItem("Átlagos idő");
+        averageTimeMenu.add(averageTimeItem);
+        
+        //winner count submenu
+        winnerCountMenu = new JMenu("Nyertesek száma");
+        statMenu.add(winnerCountMenu);
+        
+        winnerCountItem1 = new JMenuItem("Győztesek száma (5x5)");
+        winnerCountItem2 = new JMenuItem("Győztesek száma (10x10)");
+        winnerCountItem3 = new JMenuItem("Győztesek száma (15x15)");
+        winnerCountItem4 = new JMenuItem("Győztesek száma (összes típuson)");
+        
+        winnerCountMenu.add(winnerCountItem1);
+        winnerCountMenu.add(winnerCountItem2);
+        winnerCountMenu.add(winnerCountItem3);
+        winnerCountMenu.add(winnerCountItem4);
+        
+        //most winner submenu
+        mostWinnerMenu = new JMenu("Legtöbbet nyerő játékosok");
+        statMenu.add(mostWinnerMenu);
+        
+        mostWinnerItem1 = new JMenuItem("Legtöbbet nyerő (5x5)");
+        mostWinnerItem2 = new JMenuItem("Legtöbbet nyerő (10x10)");
+        mostWinnerItem3 = new JMenuItem("Legtöbbet nyerő (15x15)");
+        mostWinnerItem4 = new JMenuItem("Legtöbbet nyerő (összes típuson)");
+        
+        mostWinnerMenu.add(mostWinnerItem1);
+        mostWinnerMenu.add(mostWinnerItem2);
+        mostWinnerMenu.add(mostWinnerItem3);
+        mostWinnerMenu.add(mostWinnerItem4);
         this.setJMenuBar(menuBar);
 
     }
